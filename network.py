7ae9479c0784cb096fab1e9696b390a5a8cb320d
manuel <manuel.delverme@gmail.com>
2020-09-22 17:37:25 +0200

    [CLUSTER] mnist

--- network.py
+++ network.py
@@ -3,15 +3,14 @@ from jax.experimental import stax as stax
 
 def make_block_net(num_outputs):
     return zip(*[
-        # stax.serial(
-        #     # stax.Dense(1024, ),
-        #     stax.Dense(32, ),
-        #     stax.LeakyRelu,
-        # ),
         stax.serial(
-            # stax.Dense(1024, ),
-            stax.Dense(32, ),
-            # stax.Identity,
+            stax.Dense(1024, ),
+            #     stax.Dense(32, ),
+            stax.LeakyRelu,
+        ),
+        stax.serial(
+            stax.Dense(1024, ),
+            # stax.Dense(32, ),
             stax.LeakyRelu,
         ),
         stax.serial(
