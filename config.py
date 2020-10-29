1342abca8656c579251b6d7fc88b28b42d27ee74
manuel <manuel.delverme@gmail.com>
2020-10-29 16:24:23 +0100

    [CLUSTER]50x1

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ initial_lr_x = .01
 initial_lr_y = .01
 # 1e-2  # high lr_y make the lagrangian more responsive to sign changes -> less oscillation around 0
 
-blocks = [2, ] * 25
+blocks = [50, ] * 1
 
 
 def state_fn(x):
