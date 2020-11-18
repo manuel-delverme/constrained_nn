b97e19901d9c675265f8982b23e71a3ab7b52290
manuel <manuel.delverme@gmail.com>
2020-11-18 12:01:40 +0100

    [CLUSTER] 2134_net

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import sys
 import jax.experimental.optimizers
 import mila_tools
 
-RUN_SWEEP = 1
+RUN_SWEEP = 0
 CLUSTER = 1
 # PROJECT_NAME = "constrained_nn"
 
@@ -21,10 +21,10 @@ initial_lr_y = .08
 
 num_hidden = 256
 # blocks = [5, ] * 3
-block0 = 6
-block1 = 4
-block2 = 2
-block3 = 2
+block0 = 2
+block1 = 1
+block2 = 3
+block3 = 4
 
 use_adam = False
 grad_clip = 4.0  # avoid leaky_grad explosions
@@ -34,7 +34,7 @@ adam2 = 0.99
 batch_size = 128
 weight_norm = False  # avoid unbound targets
 
-num_epochs = 10000  # 00
+num_epochs = 100_000  # 00
 eval_every = math.ceil(num_epochs / 10000)
 
 decay_steps = num_epochs  # // 4  # 500000
@@ -54,6 +54,4 @@ lr_x = jax.experimental.optimizers.inverse_time_decay(initial_lr_x, decay_steps,
 lr_y = jax.experimental.optimizers.inverse_time_decay(initial_lr_y, decay_steps, decay_factor, staircase=True)
 blocks = [int(b) for b in [block0, block1, block2, block3] if b > 0]
 
-tb = mila_tools.deploy(cluster=CLUSTER, sweep_yaml=sweep_yaml, extra_slurm_headers="""
-#SBATCH --mem=24GB
-""")
+tb = mila_tools.deploy(cluster=CLUSTER, sweep_yaml=sweep_yaml)
