afa52cab1679c8a1d2e839deec8dd7c68ee802d2
manuel <manuel.delverme@gmail.com>
2021-02-10 20:56:20 +0100

    [CLUSTER] deployment

--- pytorch/network.py
+++ pytorch/network.py
@@ -1,48 +1,70 @@
 import torch
+
+import tqdm
 from torch import nn as nn
 from torch.nn import functional as F
 
 
 class ConstrNetwork(nn.Module):
-    def __init__(self, dataset_size):
+    def __init__(self, train_loader):
         super().__init__()
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
+        # self.dropout1 = nn.Dropout(0.25)
+        # self.dropout2 = nn.Dropout(0.5)
+
         self.fc1 = nn.Linear(9216, 128)
         self.fc2 = nn.Linear(128, 10)
-        self.states = nn.ParameterList([
-            nn.Parameter(torch.zeros(dataset_size, 9216), requires_grad=True),
-            nn.Parameter(torch.zeros(dataset_size, 128), requires_grad=True),
-        ])
-        self.multipliers = nn.ParameterList([
-            nn.Parameter(torch.zeros(dataset_size, 9216), requires_grad=True),
-            nn.Parameter(torch.zeros(dataset_size, 128), requires_grad=True),
-        ])
 
-    def step(self, x0, indices):
-        x1, x2 = self.states
+        # raise NotImplementedError("this is too big")
+        # self.states = nn.ParameterList([
+        #     nn.Parameter(torch.zeros(dataset_size, 9216), requires_grad=True),
+        #     nn.Parameter(torch.zeros(dataset_size, 128), requires_grad=True),
+        # ])
+        dataset_size = len(train_loader.dataset)
+        # self.states = nn.ModuleList([
+        #     nn.Embedding(dataset_size, 128, _weight=self.block1(train_loader.dataset), sparse=True),
+        # ])
+        weight = torch.zeros(dataset_size, 128)
+        with torch.no_grad():
+            for batch_idx, (data, target, indices) in tqdm.tqdm(enumerate(train_loader), total=len(train_loader)):
+                x_i = self.block1(data)
+                weight[indices] = x_i  # + torch.randn(x_i.shape) * 0.01
+        self.x1 = nn.Sequential(
+            nn.Embedding(dataset_size, 128, _weight=weight, sparse=True),
+            nn.ReLU()
+        )
+        print("initd")
+        # raise NotImplementedError("this is too big")
+        # self.multipliers = nn.ParameterList([
+        #     nn.Parameter(torch.zeros(dataset_size, 9216), requires_grad=True),
+        #     nn.Parameter(torch.zeros(dataset_size, 128), requires_grad=True),
+        # ])
+        # self.multipliers = nn.ModuleList([nn.Embedding(dataset_size, 128, sparse=True)])
+
+    def step(self, x0, states):
+        # x1, x2 = self.states
+        x2, = states
         return (
             self.block1(x0),
-            self.block2(x1[indices]),
-            self.block3(x2[indices])
+            # self.block2(x1),
+            self.block3(x2)
         )
 
     def forward(self, x0, indices):
-        x = self.step(x0, indices)
-        states = [x[indices] for x in self.states]
-        multi = [l[indices] for l in self.multipliers]
+        x1_target = self.x1(indices)
 
-        x_T = x[-1]
-        x_i = x[:-1]
-        h = [a - b for a, b in zip(x[:-1], states)]
-        rhs = torch.stack([torch.sum(a * b) for a, b in zip(x_i, multi)])
-        return x_T, h, torch.sum(rhs), (x_i, multi)
+        x1_hat = self.block1(x0)
+        x_T = self.block3(x1_target)
 
-    def shooting_forward(self, x):
+        h = x1_hat - x1_target
+        # raise config.tb.run.log_tensor_stats(x1_hat)
+        # rhs = torch.stack([torch.sum(a * b) for a, b in zip(x_i, multi)])
+        return x_T, h
+
+    def full_rollout(self, x):
         x = self.block1(x)
-        x = self.block2(x)
+        # x = self.block2(x)
         x = self.block3(x)
         return x
 
@@ -54,7 +76,7 @@ class ConstrNetwork(nn.Module):
     def block2(self, x):
         x = self.fc1(x)
         x = F.relu(x)
-        x = self.dropout2(x)
+        # x = self.dropout2(x)
         return x
 
     def block1(self, x):
@@ -63,6 +85,7 @@ class ConstrNetwork(nn.Module):
         x = self.conv2(x)
         x = F.relu(x)
         x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
+        # x = self.dropout1(x)
         x = torch.flatten(x, 1)
+        x = self.block2(x)
         return x
