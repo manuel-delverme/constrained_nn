db89241283fc91cf0bb1b6eca80073121e424308
manuel <manuel.delverme@gmail.com>
2021-02-11 19:16:22 +0100

    training with penalty method.

--- pytorch/train.py
+++ pytorch/train.py
@@ -1,11 +1,11 @@
 import sys
 import torch
 import torch.nn.functional as F
+import torch.optim
 import torch.utils.data
-from torch.optim.lr_scheduler import StepLR
 from torchvision import datasets, transforms
 
-import pytorch.extragradient
+import pytorch.config as config
 from pytorch.network import ConstrNetwork
 
 
@@ -15,30 +15,43 @@ class MNIST(datasets.MNIST):
         return data, target, index
 
 
-def train(model, device, train_loader, optimizer, epoch):
+def train(model, device, train_loader, optimizer, epoch, step):
     model.train()
     for batch_idx, (data, target, indices) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+        data, target, indices = data.to(device), target.to(device), indices.to(device)
+        data = data  # .double()
         optimizer.zero_grad()
-        x_T, h, rhs, (x, multi) = model(data, indices)
+        # model.multipliers.requires_grad = False
+        x_T, rhs = model(data, indices)
         loss = F.nll_loss(x_T, target)
         # grad lambda = h
-        lagr = loss + rhs
+        constr_loss = rhs.pow(2).mean(1).mean(0)
+        lagr = loss + constr_loss
         lagr.backward()
-        plot(lagr, model)
-        # plot(multi, model)
-        for i, m in enumerate(multi):
-            torch.index_add()
-            model.multipliers[i].grad[indices] *= -1
-        # (-rhs).backward()
+        # clip_grad_value_(model.parameters(), 1.1)
 
-        if batch_idx % 2 == 0:
-            optimizer.extrapolation()
-        else:
-            optimizer.step()
-        if batch_idx % 10 == 0:
-            print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)}'
-                  f' ({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item():.6f}')
+        # for name, p in model.named_parameters():
+        #     if not p.grad.is_sparse:
+        #         p.grad.clamp(max=1.1)
+
+        # for i, m in enumerate(multi):
+        #     # torch.index_add()
+        #     model.multipliers[i].weight.grad *= -1
+
+        # if batch_idx % 2 == 0:
+        #     optimizer.extrapolation()
+        # else:
+        #     optimizer.step()
+        optimizer.step()
+        # if batch_idx % 10 == 0:
+        config.tb.add_scalar("train/loss", float(loss.item()), batch_idx + step)
+        config.tb.add_scalar("train/constr_loss", float(constr_loss), batch_idx + step)
+        config.tb.add_scalar("train/lagr", float(lagr.item()), batch_idx + step)
+        print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)}'
+              f' ({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item():.6f} rhs: {constr_loss.item():.6f}')
+        if constr_loss > 10:
+            sys.exit()
+    return batch_idx + step
 
 
 def plot(loss, model):
@@ -53,9 +66,10 @@ def test(model, device, test_loader):
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target in test_loader:
+        for (data, target, idx) in test_loader:
             data, target = data.to(device), target.to(device)
-            output = model(data)
+            data = data  # .double()
+            output = model.full_rollout(data)
             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
@@ -70,15 +84,14 @@ def test(model, device, test_loader):
 def main():
     # Training settings
     use_cuda = torch.cuda.is_available()
-    DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
-    use_cuda = not DEBUG
+    use_cuda = not config.DEBUG
+    use_cuda = True
 
     torch.manual_seed(1337)
-
     device = torch.device("cuda" if use_cuda else "cpu")
 
-    train_kwargs = {'batch_size': 64}
-    test_kwargs = {'batch_size': 1000}
+    train_kwargs = {'batch_size': 512}
+    test_kwargs = {'batch_size': 2048}
     if use_cuda:
         cuda_kwargs = {'num_workers': 1, 'pin_memory': True, 'shuffle': True}
         train_kwargs.update(cuda_kwargs)
@@ -93,15 +106,20 @@ def main():
     train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
-    model = ConstrNetwork(len(train_loader.dataset)).to(device)
-    # optimizer = optim.Adam(model.parameters(), lr=0.001)
-    optimizer = pytorch.extragradient.ExtraAdam(model.parameters(), lr=0.001)
+    model = ConstrNetwork(torch.utils.data.DataLoader(dataset1, batch_size=2048)).to(device)
+    # optimizer = torch.optim.Adam(list(model.parameters()), lr=0.001)
+    # optimizer = torch.optim.Adagrad(model.parameters(), lr=0.01)
+    optimizer = torch.optim.SGD(model.parameters(), lr=config.initial_lr_theta)
+    # https://discuss.pytorch.org/t/sparse-embedding-failing-with-adam-torch-cuda-sparse-floattensor-has-no-attribute-addcmul/5589/9
+    # optimizer = pytorch.extragradient.ExtraAdam(model.parameters(), lr=0.001)
 
-    scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
-    for epoch in range(0, 14):
-        train(model, device, train_loader, optimizer, epoch)
+    # scheduler = StepLR(optimizer, step_size=config.decay_steps, gamma=config.decay_factor)
+    config.tb.watch(model, criterion=None, log="all", log_freq=1)
+    step = 0
+    for epoch in range(100):
+        step = train(model, device, train_loader, optimizer, epoch, step)
         test(model, device, test_loader)
-        scheduler.step()
+        # scheduler.step()
 
 
 if __name__ == '__main__':
