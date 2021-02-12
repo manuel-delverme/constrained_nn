c7fe9e6a3d5bcd9807fee3c37ca1b44060c0747e
manuel <manuel.delverme@gmail.com>
2021-02-12 19:53:58 +0100

    [CLUSTER] big_lambda

--- pytorch/config.py
+++ pytorch/config.py
@@ -13,7 +13,8 @@ DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
 
 random_seed = 1337
 
-initial_lr_theta = .001
+initial_lr_theta = .1
+lambda_ = .1
 # initial_lr_x = .05
 # initial_lr_y = .08
 # high lr_y make the lagrangian more responsive to sign changes -> less oscillation around 0
@@ -34,4 +35,4 @@ device = torch.device("cuda" if use_cuda else "cpu")
 ################################################################
 tb = mila_tools.deploy(host=HOST, sweep_yaml=sweep_yaml, extra_slurm_headers="""
 #SBATCH --mem=24GB
-""")
+""", proc_num=10)
