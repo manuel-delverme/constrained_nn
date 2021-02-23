73af180a610736884ed2540be88d69c325616e5a
manuel <manuel.delverme@gmail.com>
2021-02-23 18:55:04 +0100

    per parameter LR

--- pytorch/config.py
+++ pytorch/config.py
@@ -12,17 +12,17 @@ DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
 
 random_seed = 1337
 
-initial_lr_theta = 0.01
+initial_lr_theta = .005186
+initial_lr_x = .04887
+initial_lr_y = .09284
 
 warmup_lr = 0.009185
 lambda_ = 0.06788
-# initial_lr_x = .05
-# initial_lr_y = .08
 # high lr_y make the lagrangian more responsive to sign changes -> less oscillation around 0
 
 batch_size = 1024
 warmup_epochs = 0 if DEBUG else 150
-num_epochs = 1000
+num_epochs = 50
 initial_forward = not DEBUG
 
 use_cuda = not DEBUG
