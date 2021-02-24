be435a98a5794eb18be0f5cf022e1d02ba797547
manuel <manuel.delverme@gmail.com>
2021-02-24 20:42:31 +0100

    [CLUSTER] high_y!

--- pytorch/config.py
+++ pytorch/config.py
@@ -3,25 +3,27 @@ import torch
 
 import mila_tools
 
-RUN_SWEEP = 1
+RUN_SWEEP = 0
 REMOTE = 1
+NUM_PROCS = 1
 
 sweep_yaml = "pytorch/sweep_hyper.yaml" if RUN_SWEEP else False
 HOST = "mila" if REMOTE else ""
 DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
+DEBUG = False
 
 random_seed = 1337
 
 initial_lr_theta = .005186
 initial_lr_x = .04887
-initial_lr_y = .09284
+initial_lr_y = 1.
 
 warmup_lr = 0.009185
 lambda_ = 0.06788
 # high lr_y make the lagrangian more responsive to sign changes -> less oscillation around 0
 
 batch_size = 1024
-warmup_epochs = 1 if DEBUG else 150
+warmup_epochs = 5  # 1 if DEBUG else 100  # 150
 num_epochs = 50
 initial_forward = not DEBUG
 
@@ -40,4 +42,5 @@ device = torch.device("cuda" if use_cuda else "cpu")
 # #SBATCH --mem=24GB
 # """
 esh = ""
-tb = mila_tools.deploy(host=HOST, sweep_yaml=sweep_yaml, extra_slurm_headers=esh, proc_num=10)
+#SBATCH --partition=long
+tb = mila_tools.deploy(host=HOST, sweep_yaml=sweep_yaml, extra_slurm_headers=esh, proc_num=NUM_PROCS)
