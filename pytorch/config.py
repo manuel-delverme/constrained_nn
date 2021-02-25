5dc85eecee3b2114f0249987a56f493a19557055
manuel <manuel.delverme@gmail.com>
2021-02-25 10:45:32 +0100

    [CLUSTER] warmup+adv

--- pytorch/config.py
+++ pytorch/config.py
@@ -3,14 +3,13 @@ import torch
 
 import mila_tools
 
-RUN_SWEEP = 0
+RUN_SWEEP = 1
 REMOTE = 1
-NUM_PROCS = 1
+NUM_PROCS = 10
 
 sweep_yaml = "pytorch/sweep_hyper.yaml" if RUN_SWEEP else False
 HOST = "mila" if REMOTE else ""
 DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
-DEBUG = False
 
 random_seed = 1337
 
@@ -23,7 +22,7 @@ lambda_ = 0.06788
 # high lr_y make the lagrangian more responsive to sign changes -> less oscillation around 0
 
 batch_size = 1024
-warmup_epochs = 1 if DEBUG else 5  # 150
+warmup_epochs = 1 if DEBUG else 50
 num_epochs = 50
 initial_forward = not DEBUG
 
