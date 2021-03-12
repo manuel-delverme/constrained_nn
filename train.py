df7fbde28af7c2873ae1b59d1127fb4320c753e6
manuel <manuel.delverme@gmail.com>
2021-03-12 15:32:54 +0100

    [CLUSTER] tanh!

--- train.py
+++ train.py
@@ -46,7 +46,7 @@ def train(model, device, train_loader, optimizer, epoch, step, adversarial, aux_
             optimizer.extrapolation()
 
             # Player 2
-            model.multipliers.weight.grad = torch.sparse_coo_tensor(indices.unsqueeze(0), -rhs, model.multipliers.weight.shape)
+            model.multipliers[0].weight.grad = torch.sparse_coo_tensor(indices.unsqueeze(0), -rhs, model.multipliers[0].weight.shape)
             aux_optimizer.extrapolation()
 
             optimizer.zero_grad()
@@ -68,7 +68,7 @@ def train(model, device, train_loader, optimizer, epoch, step, adversarial, aux_
             optimizer.step()
 
             # Player 2
-            model.multipliers.weight.grad = torch.sparse_coo_tensor(indices.unsqueeze(0), -rhs, model.multipliers.weight.grad.shape)
+            model.multipliers[0].weight.grad = torch.sparse_coo_tensor(indices.unsqueeze(0), -rhs, model.multipliers[0].weight.grad.shape)
             aux_optimizer.step()
 
             with torch.no_grad():
