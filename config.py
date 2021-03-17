8014e8aafa7d1645018c9d733266864b124dd1d0
manuel <manuel.delverme@gmail.com>
2021-03-17 19:02:12 +0100

    pretty config

--- config.py
+++ config.py
@@ -4,12 +4,9 @@ import torch
 
 import experiment_buddy
 
-RUN_SWEEP = 0
+RUN_SWEEP = 1
 REMOTE = 1
-NUM_PROCS = 10 if RUN_SWEEP else 1
 
-sweep_yaml = "sweep_hyper.yaml" if RUN_SWEEP else False
-HOST = "mila" if REMOTE else ""
 DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
 dataset_path = "../data" if DEBUG else "/network/datasets/mnist.var/mnist_torchvision/"
 
@@ -18,7 +15,6 @@ random_seed = 1337
 initial_lr_theta = .001
 initial_lr_x = .05
 initial_lr_y = .08
-
 # 1e-2  # high lr_y make the lagrangian more responsive to sign changes -> less oscillation around 0
 
 warmup_lr = 0.009185
@@ -41,8 +37,11 @@ device = torch.device("cuda" if use_cuda else "cpu")
 ################################################################
 # Derivative parameters
 ################################################################
-# esh = """
-# #SBATCH --mem=24GB
-# """
-esh = ""
-tb = experiment_buddy.deploy(host=HOST, sweep_yaml=sweep_yaml, extra_slurm_headers=esh, proc_num=NUM_PROCS)
+tb = experiment_buddy.deploy(
+    host="mila" if REMOTE else "",
+    sweep_yaml="sweep_hyper.yaml" if RUN_SWEEP else False,
+    extra_slurm_headers="""
+    """,
+    # SBATCH --mem=24GB
+    proc_num=10 if RUN_SWEEP else 1
+)
