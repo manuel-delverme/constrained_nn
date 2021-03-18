0812b50fdc8f8d0004d8e4659a0d7f10c62d6734
manuel <manuel.delverme@gmail.com>
2021-03-18 09:27:55 +0100

    [CLUSTER] relu-constr

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import torch
 
 import experiment_buddy
 
-RUN_SWEEP = 1
+RUN_SWEEP = 0
 REMOTE = 1
 
 DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
@@ -17,7 +17,7 @@ initial_lr_x = .05
 initial_lr_y = .08
 # 1e-2  # high lr_y make the lagrangian more responsive to sign changes -> less oscillation around 0
 
-warmup_lr = 0.009185
+warmup_lr = 0.009186
 lambda_ = 0.06788
 # high lr_y make the lagrangian more responsive to sign changes -> less oscillation around 0
 
