62772fcd9f6fa71dfff29490950be30c81e075f0
manuel <manuel.delverme@gmail.com>
2021-03-18 09:56:34 +0100

    [CLUSTER] chance_constraint

--- config.py
+++ config.py
@@ -17,14 +17,14 @@ initial_lr_x = .05
 initial_lr_y = .08
 # 1e-2  # high lr_y make the lagrangian more responsive to sign changes -> less oscillation around 0
 
-warmup_lr = 0.009185
+warmup_lr = 0.009186
 lambda_ = 0.06788
 # high lr_y make the lagrangian more responsive to sign changes -> less oscillation around 0
 
 batch_size = 1024
 warmup_epochs = 1 if DEBUG else 50
 num_epochs = 150
-constr_margin = 0.1
+constr_margin = 0.01
 initial_forward = not DEBUG
 use_cuda = not DEBUG
 
