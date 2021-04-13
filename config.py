48b5e51e7ecc0e29df5d160f5ff609068b138e14
manuel <manuel.delverme@gmail.com>
2021-04-13 16:07:35 +0200

    [CLUSTER] 0.1corruption

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
 import sys
-
 import torch
 
 import experiment_buddy
 
-RUN_SWEEP = 1
+RUN_SWEEP = 0
 REMOTE = 1
 
 DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
 dataset_path = "../data" if DEBUG else "/network/datasets/{}.var/{}_torchvision"
 
-corruption_percentage = 0.00
+corruption_percentage = 0.1
 soft_eps = False
 
 chance_constraint = 0.05
@@ -28,7 +27,7 @@ lambda_ = 0.06788
 
 batch_size = 1024
 warmup_epochs = 1 if DEBUG else 5
-num_epochs = 150
+num_epochs = 100
 initial_forward = not DEBUG
 use_cuda = not DEBUG
 
@@ -47,5 +46,5 @@ tb = experiment_buddy.deploy(
     extra_slurm_headers="""
     """,
     # SBATCH --mem=24GB
-    proc_num=10 if RUN_SWEEP else 1
+    proc_num=1 if RUN_SWEEP else 1
 )
