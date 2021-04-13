b978b884cac8417a55b90cf977f480c90d535978
manuel <manuel.delverme@gmail.com>
2021-04-13 15:54:46 +0200

    [CLUSTER] corrupt_train

--- cifair_train.py
+++ cifair_train.py
@@ -12,64 +12,92 @@ import extragradient
 import network
 
 
-class MNIST(datasets.MNIST):
+class CIFAR10(datasets.CIFAR10):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if config.DEBUG:
             self.data, self.targets = self.data[:config.batch_size * 2], self.targets[:config.batch_size * 2]
-        if config.corruption_percentage and kwargs['train']:
-            num_corrupted_indices = int(config.corruption_percentage * len(self.data))
-            indices = torch.randint(0, len(self.data) - 1, (num_corrupted_indices,))
-            self.data[indices] = torch.randint_like(self.data[indices], self.data.max())
+        num_corrupted_indices = int(config.corruption_percentage * len(self.data))
+        indices = torch.randint(0, len(self.data) - 1, (num_corrupted_indices,))
+        self.data[indices] = torch.randint_like(self.data[indices], self.data.max())
 
     def __getitem__(self, index):
         data, target = super().__getitem__(index)
         return data, target, index
 
 
-def train(model, device, train_loader, optimizer, epoch, step, aux_optimizer=None):
+def train(model, device, train_loader, optimizer, epoch, step, adversarial, aux_optimizer=None):
     model.train()
     for batch_idx, (data, target, indices) in enumerate(train_loader):
         data, target, indices = data.to(device), target.to(device), indices.to(device)
+        optimizer.zero_grad()
+        x_T, rhs = model(data, indices)
+        loss = F.nll_loss(x_T, target)
 
-        # opt_step(aux_optimizer, batch_idx, data, epoch, indices, model, optimizer, step, target, extrapolate=True)
-        opt_step(aux_optimizer, batch_idx, data, epoch, indices, model, optimizer, step, target, extrapolate=False)
-        print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} ({100. * batch_idx / len(train_loader):.0f}%)]\t')
-
-    return batch_idx + step
-
-
-def opt_step(aux_optimizer, batch_idx, data, epoch, indices, model, optimizer, step, target, extrapolate=False):
-    optimizer.zero_grad()
-    y_hat, sample_weights, rhs = model(data, indices)
-    loss = F.nll_loss(y_hat, target, reduce=False) * sample_weights.squeeze()
-    loss = loss.mean()
-    # Extrapolation
-    constr_loss = torch.einsum('bh,bh->', model.multipliers(indices), rhs)
-    lagrangian = loss * constr_loss
-
-    lagrangian.backward()  # Player 1
-
-    if extrapolate:
-        optimizer.extrapolation()
-    else:
-        optimizer.step()
-
-    # Player 2
-    model.multipliers[0].weight.grad = torch.sparse_coo_tensor(indices.unsqueeze(0), -rhs, model.multipliers[0].weight.shape)
-
-    if extrapolate:
-        aux_optimizer.extrapolation()
-    else:
-        aux_optimizer.step()
-
-    if extrapolate:
         config.tb.add_scalar("train/loss", float(loss.item()), batch_idx + step)
         config.tb.add_scalar("train/mean_defect", float(rhs.mean()), batch_idx + step)
+        config.tb.add_scalar("train/adversarial", float(adversarial), batch_idx + step)
         config.tb.add_scalar("train/epoch", epoch, batch_idx + step)
-        config.tb.add_scalar("train/lambda_h", float(constr_loss), batch_idx + step)
-        config.tb.add_scalar("train/lagrangian0", lagrangian, batch_idx + step)
-    return y_hat
+
+        if adversarial:
+            # Extrapolation
+
+            constr_loss = torch.einsum('bh,bh->', model.multipliers(indices), rhs)
+            config.tb.add_scalar("train/constr_loss", float(constr_loss), batch_idx + step)
+
+            lagr = loss + constr_loss
+            config.tb.add_scalar("train/lagrangian0", lagr, batch_idx + step)
+            aug_lagr = lagr  # + config.lambda_ * rhs.pow(2).mean(1).mean(0)
+
+            aug_lagr.backward()  # Player 1
+            optimizer.extrapolation()
+
+            # Player 2
+            model.multipliers[0].weight.grad = torch.sparse_coo_tensor(indices.unsqueeze(0), -rhs, model.multipliers[0].weight.shape)
+            aux_optimizer.extrapolation()
+
+            optimizer.zero_grad()
+            aux_optimizer.zero_grad()
+            # Step
+            # Eval
+            x_T, rhs = model(data, indices)
+            loss = F.nll_loss(x_T, target)
+
+            # Loss
+            constr_loss = torch.einsum('bh,bh->', model.multipliers(indices).squeeze(), rhs)
+            lagr = loss + constr_loss
+            config.tb.add_scalar("train/lagrangian1", lagr, batch_idx + step)
+
+            aug_lagr = lagr + config.lambda_ * rhs.pow(2).mean(1).mean(0)
+
+            # Grads
+            aug_lagr.backward()  # Player 1
+            optimizer.step()
+
+            # Player 2
+            model.multipliers[0].weight.grad = torch.sparse_coo_tensor(indices.unsqueeze(0), -rhs, model.multipliers[0].weight.grad.shape)
+            aux_optimizer.step()
+
+            with torch.no_grad():
+                # Eval
+                x_T, rhs = model(data, indices)
+                loss = F.nll_loss(x_T, target)
+
+                # Loss
+                constr_loss = torch.einsum('bh,bh->', model.multipliers(indices).squeeze(), rhs)
+                lagr = loss + constr_loss
+                config.tb.add_scalar("train/lagrangian-1", lagr, batch_idx + step)
+        else:
+            constr_loss = config.lambda_ * rhs.pow(2).mean(1).mean(0)
+            lagr = loss + constr_loss
+            lagr.backward()
+            optimizer.step()
+
+        print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)}'
+              f' ({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item():.6f} rhs: {constr_loss.item():.6f}')
+        # if constr_loss > 10:
+        #     sys.exit()
+    return batch_idx + step
 
 
 def grad_step(aux_optimizer, batch_idx, data, indices, model, optimizer, step, target):
@@ -134,20 +162,33 @@ def main():
         transforms.Normalize((0.1307,), (0.3081,))
     ])
     if "SLURM_JOB_ID" in os.environ.keys():
-        dataset1 = MNIST(config.dataset_path.format("mnist", "mnist"), train=True, transform=transform)
-        dataset2 = MNIST(config.dataset_path.format("mnist", "mnist"), train=False, transform=transform)
+        dataset1 = CIFAR10(config.dataset_path.format("cifar10", "cifar10"), train=True, transform=transform)
+        dataset2 = CIFAR10(config.dataset_path.format("cifar10", "cifar10"), train=False, transform=transform)
     else:
-        dataset1 = MNIST("../data", train=True, transform=transform)
-        dataset2 = MNIST("../data", train=False, transform=transform)
+        dataset1 = CIFAR10("../data", train=True, transform=transform)
+        dataset2 = CIFAR10("../data", train=False, transform=transform)
 
     train_loader = torch.utils.data.DataLoader(dataset1, shuffle=True, **train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
     model = network.ConstrNetwork(
         torch.utils.data.DataLoader(dataset1, batch_size=test_kwargs['batch_size'])).to(config.device)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=config.initial_lr_theta)
+    # optimizer = torch.optim.Adagrad(model.parameters(), lr=0.01)
+    # optimizer = torch.optim.SGD(model.parameters(), lr=config.initial_lr_theta)
+    # https://discuss.pytorch.org/t/sparse-embedding-failing-with-adam-torch-cuda-sparse-floattensor-has-no-attribute-addcmul/5589/9
 
+    # config.tb.watch(model, criterion=None, log="all", log_freq=10)
     step = 0
+    optimizer = torch.optim.Adagrad(model.parameters(), lr=config.warmup_lr)
+    # scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
+    print("WARMUP")
+    for epoch in range(config.warmup_epochs):
+        step = train(model, config.device, train_loader, optimizer, epoch, step, adversarial=False)
+        test(model, config.device, test_loader, step)
+        # scheduler.step(epoch)
 
+    print("Adversarial")
     theta = [v for k, v in model.named_parameters() if not k.startswith("x1") and not k.startswith("multipliers")]
     x = [v for k, v in model.named_parameters() if k.startswith("x1")]
     multi = [v for k, v in model.named_parameters() if k.startswith("multipliers")]
@@ -160,7 +201,7 @@ def main():
 
     # scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
     for epoch in range(config.num_epochs):
-        step = train(model, config.device, train_loader, optimizer, epoch, step, aux_optimizer=aux_optimizer)
+        step = train(model, config.device, train_loader, optimizer, epoch, step, adversarial=True, aux_optimizer=aux_optimizer)
         test(model, config.device, test_loader, step)
         # scheduler.step(epoch)
 
