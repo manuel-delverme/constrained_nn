2324e7c247ade4d6329fe608f6407be51fcf6d32
manuel <manuel.delverme@gmail.com>
2021-04-19 03:58:12 +0200

    [CLUSTER] zerograd

--- train.py
+++ train.py
@@ -10,32 +10,52 @@ import network
 import utils
 
 
-def train(model, device, train_loader, optimizer, epoch, step, adversarial, aux_optimizer=None):
+def train(model, device, train_loader, optimizer, epoch, step, adversarial, update_weights, aux_optimizer=None):
     model.train()
+    if not adversarial:
+        raise NotImplemented
+
     for batch_idx, (data, target, indices) in enumerate(train_loader):
         data, target, indices = data.to(device), target.to(device), indices.to(device)
 
-        opt_step(aux_optimizer, batch_idx, data, epoch, indices, model, optimizer, step, target, extrapolate=True)
-        opt_step(aux_optimizer, batch_idx, data, epoch, indices, model, optimizer, step, target, extrapolate=False)
-        print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} ({100. * batch_idx / len(train_loader):.0f}%)]\t')
+        # optimizer.zero_grad
+        # aux_optimizer.zero_grad
 
+        # forward
+        # loss
+
+        # costr losses
+        # L = loss + costrloss
+
+        # (lagr + config.lambda_ * rhs.pow(2).mean(1).mean(0)).backward()
+        # optimizer.step()
+
+        # multi.grad = -rhs
+        # aux_optimizer.step
+
+        opt_step(aux_optimizer, batch_idx, data, epoch, indices, model, optimizer, step, target, update_weights, extrapolate=True)
+        opt_step(aux_optimizer, batch_idx, data, epoch, indices, model, optimizer, step, target, update_weights, extrapolate=False)
+        print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} ({100. * batch_idx / len(train_loader):.0f}%)]\t')
     return batch_idx + step
 
 
-def opt_step(aux_optimizer, batch_idx, data, epoch, indices, model, optimizer, step, target, extrapolate=False):
+def opt_step(aux_optimizer, batch_idx, data, epoch, indices, model, optimizer, step, target, update_weights, extrapolate=False):
     optimizer.zero_grad()
+    aux_optimizer.zero_grad()
 
-    if config.experiment == "target_prop":
-        y_hat, rhs = model(data, indices)
-        loss = F.nll_loss(y_hat, target)
-    else:
-        y_hat, sample_weights, rhs = model(data, indices)
-        loss = F.nll_loss(y_hat, target, reduce=False) * sample_weights.squeeze()
-        loss = loss.mean()
+    rhs, loss, defect = forward_step(data, indices, model, target)
+    # loss.backward(retain_graph=True)
+    # unconstrained_loss_grad_norm = torch.norm(torch.stack([torch.norm(p.grad.detach(), 2) for p in model.parameters() if p.grad is not None]), 2)
+    # optimizer.zero_grad()
 
-    # Extrapolation
-    constr_loss = torch.einsum('bh,bh->', model.multipliers(indices), rhs)
-    lagrangian = loss + constr_loss
+    # rhs.backward(retain_graph=True)
+    # constraint_satisfaction_grad_norm = torch.norm(torch.stack([torch.norm(p.grad.detach(), 2) for p in model.parameters() if p.grad is not None]), 2)
+    #  optimizer.zero_grad()
+
+    lagrangian = loss + rhs
+
+    if not extrapolate:
+        lagrangian = lagrangian + config.lambda_ * defect.pow(2).mean(1).mean(0)
 
     lagrangian.backward()  # Player 1
 
@@ -43,28 +63,47 @@ def opt_step(aux_optimizer, batch_idx, data, epoch, indices, model, optimizer, s
         optimizer.extrapolation()
     else:
         optimizer.step()
+    if config.adversarial_sampling:
+        update_weights()
 
     # Player 2
-    model.multipliers[0].weight.grad = torch.sparse_coo_tensor(indices.unsqueeze(0), -rhs, model.multipliers[0].weight.shape)
+    model.multipliers[0].weight.grad = torch.sparse_coo_tensor(indices.unsqueeze(0), -defect, model.multipliers[0].weight.shape)
+    #  constr_grad_norm = torch.norm(torch.stack(
+    #      [torch.norm(p.grad.detach(), 2) for p in rhs.parameters() if p.grad is not None]
+    #  ), 2)
 
     if extrapolate:
         aux_optimizer.extrapolation()
     else:
         aux_optimizer.step()
 
-    if extrapolate:
-        metrics = {
-            "train/loss": float(loss.item()),
-            "train/mean_defect": float(rhs.mean()),
-            "train/epoch": epoch,
-            "train/lambda_h": float(constr_loss),
-            "train/lagrangian0": lagrangian
-        }
-        for k, v in metrics.items():
-            if extrapolate:
-                k += "_ext"
-            config.tb.add_scalar(k, v, batch_idx + step)
-    return y_hat
+    metrics = {
+        "train/loss": float(loss.item()),
+        "train/l1_defect": float(defect.abs().sum()),
+        "train/epoch": epoch,
+        "train/rhs": float(rhs),
+        # "train/unconstrained_loss_grad_norm": float(unconstrained_loss_grad_norm),
+        # "train/constraint_satisfaction_grad_norm": float(constraint_satisfaction_grad_norm),
+        "train/lagrangian": lagrangian
+    }
+    for k, v in metrics.items():
+        if extrapolate:
+            k += "_ext"
+        config.tb.add_scalar(k, v, batch_idx + step)
+    config.tb.add_histogram("train/indices", indices.cpu(), batch_idx + step)
+
+
+def forward_step(data, indices, model, target):
+    if config.experiment == "target_prop":
+        y_hat, defect = model(data, indices)
+        loss = F.nll_loss(y_hat, target)
+    else:
+        y_hat, sample_weights, defect = model(data, indices)
+        loss = F.nll_loss(y_hat, target, reduce=False) * sample_weights.squeeze()
+        loss = loss.mean()
+    # Extrapolation
+    rhs = torch.einsum('bh,bh->', model.multipliers(indices), defect)
+    return rhs, loss, defect
 
 
 def grad_step(aux_optimizer, batch_idx, data, indices, model, optimizer, step, target):
@@ -115,6 +154,13 @@ def main():
         raise NotImplemented
         model = network.ConstrNetwork(train_loader).to(config.device)
 
+    @torch.no_grad()
+    def update_weights():
+        if config.adversarial_sampling:
+            train_loader.sampler.weights = torch.softmax(torch.max(model.multipliers[0].weight, dim=-1).values, dim=0)
+
+    update_weights()
+
     step = 0
 
     # print("WARMUP")
@@ -135,7 +181,7 @@ def main():
     aux_optimizer = extragradient.ExtraSGD([{'params': multi, 'lr': config.initial_lr_y}])
 
     for epoch in range(config.num_epochs):
-        step = train(model, config.device, train_loader, optimizer, epoch, step, adversarial=True, aux_optimizer=aux_optimizer)
+        step = train(model, config.device, train_loader, optimizer, epoch, step, adversarial=True, aux_optimizer=aux_optimizer, update_weights=update_weights)
         test(model, config.device, test_loader, step)
 
 
