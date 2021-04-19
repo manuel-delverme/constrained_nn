5ffb9b266c4f399219f6a4c92ffa4961ee0d0967
manuel <manuel.delverme@gmail.com>
2021-04-19 07:16:39 +0200

    [CLUSTER] pre_augremoval

--- train.py
+++ train.py
@@ -12,76 +12,81 @@ import utils
 
 def train(model, device, train_loader, optimizer, epoch, step, adversarial, aux_optimizer=None):
     model.train()
+
     for batch_idx, (data, target, indices) in enumerate(train_loader):
         data, target, indices = data.to(device), target.to(device), indices.to(device)
-        optimizer.zero_grad()
-        x_T, rhs = model(data, indices)
-        loss = F.nll_loss(x_T, target)
-
-        config.tb.add_scalar("train/loss", float(loss.item()), batch_idx + step)
-        config.tb.add_scalar("train/mean_defect", float(rhs.mean()), batch_idx + step)
-        config.tb.add_scalar("train/adversarial", float(adversarial), batch_idx + step)
         config.tb.add_scalar("train/epoch", epoch, batch_idx + step)
+        config.tb.add_scalar("train/adversarial", float(adversarial), batch_idx + step)
 
         if adversarial:
             # Extrapolation
+            optimizer.zero_grad()
+            aux_optimizer.zero_grad()
 
-            constr_loss = torch.einsum('bh,bh->', model.multipliers(indices), rhs)
-            config.tb.add_scalar("train/constr_loss", float(constr_loss), batch_idx + step)
+            rhs, loss, defect = forward_step(data, indices, model, target)
 
-            lagr = loss + constr_loss
-            config.tb.add_scalar("train/lagrangian0", lagr, batch_idx + step)
-            aug_lagr = lagr  # + config.lambda_ * rhs.pow(2).mean(1).mean(0)
+            config.tb.add_scalar("train/loss", float(loss.item()), batch_idx + step)
+            config.tb.add_scalar("train/mean_defect", float(defect.mean()), batch_idx + step)
+            config.tb.add_scalar("train/mean_defect", float(rhs.mean()), batch_idx + step)
 
-            aug_lagr.backward()  # Player 1
+            (loss + rhs).backward()
             optimizer.extrapolation()
 
             # Player 2
-            model.multipliers[0].weight.grad = torch.sparse_coo_tensor(indices.unsqueeze(0), -rhs, model.multipliers[0].weight.shape)
+            model.multipliers[0].weight.grad = torch.sparse_coo_tensor(indices.unsqueeze(0), -defect, model.multipliers[0].weight.shape)
             aux_optimizer.extrapolation()
 
             optimizer.zero_grad()
             aux_optimizer.zero_grad()
+
             # Step
-            # Eval
-            x_T, rhs = model(data, indices)
-            loss = F.nll_loss(x_T, target)
+            rhs, loss, defect = forward_step(data, indices, model, target)
 
             # Loss
-            constr_loss = torch.einsum('bh,bh->', model.multipliers(indices).squeeze(), rhs)
-            lagr = loss + constr_loss
-            config.tb.add_scalar("train/lagrangian1", lagr, batch_idx + step)
-
-            aug_lagr = lagr + config.lambda_ * rhs.pow(2).mean(1).mean(0)
+            lagr = loss + rhs
+            aug_lagr = lagr + config.lambda_ * defect.pow(2).mean(1).mean(0)
 
             # Grads
             aug_lagr.backward()  # Player 1
             optimizer.step()
 
             # Player 2
-            model.multipliers[0].weight.grad = torch.sparse_coo_tensor(indices.unsqueeze(0), -rhs, model.multipliers[0].weight.grad.shape)
+            model.multipliers[0].weight.grad = torch.sparse_coo_tensor(indices.unsqueeze(0), -defect, model.multipliers[0].weight.grad.shape)
             aux_optimizer.step()
-
-            with torch.no_grad():
-                # Eval
-                x_T, rhs = model(data, indices)
-                loss = F.nll_loss(x_T, target)
-
-                # Loss
-                constr_loss = torch.einsum('bh,bh->', model.multipliers(indices).squeeze(), rhs)
-                lagr = loss + constr_loss
-                config.tb.add_scalar("train/lagrangian-1", lagr, batch_idx + step)
         else:
-            constr_loss = config.lambda_ * rhs.pow(2).mean(1).mean(0)
+            optimizer.zero_grad()
+            aux_optimizer.zero_grad()
+
+            rhs, loss, defect = forward_step(data, indices, model, target)
+            config.tb.add_scalar("train/loss", float(loss.item()), batch_idx + step)
+            config.tb.add_scalar("train/mean_defect", float(defect.mean()), batch_idx + step)
+            config.tb.add_scalar("train/rhs_defect", float(rhs.mean()), batch_idx + step)
+
+            constr_loss = config.lambda_ * defect.pow(2).mean(1).mean(0)
             lagr = loss + constr_loss
             lagr.backward()
             optimizer.step()
 
         print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)}'
-              f' ({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item():.6f} rhs: {constr_loss.item():.6f}')
-        # if constr_loss > 10:
-        #     sys.exit()
+              f' ({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item():.6f} rhs: {rhs.item():.6f}')
+
+    if config.adversarial_sampling:
+        with torch.no_grad():
+            train_loader.sampler.weights = torch.softmax(torch.max(model.multipliers[0].weight, dim=-1).values, dim=0)
     return batch_idx + step
+
+
+def forward_step(data, indices, model, target):
+    if config.experiment == "target_prop":
+        y_hat, defect = model(data, indices)
+        loss = F.nll_loss(y_hat, target)
+    else:
+        y_hat, sample_weights, defect = model(data, indices)
+        loss = F.nll_loss(y_hat, target, reduce=False) * sample_weights.squeeze()
+        loss = loss.mean()
+    # Extrapolation
+    rhs = torch.einsum('bh,bh->', model.multipliers(indices), defect)
+    return rhs, loss, defect
 
 
 def grad_step(aux_optimizer, batch_idx, data, indices, model, optimizer, step, target):
@@ -131,13 +136,6 @@ def main():
     else:
         raise NotImplemented
         model = network.ConstrNetwork(train_loader).to(config.device)
-
-    @torch.no_grad()
-    def update_weights():
-        if config.adversarial_sampling:
-            train_loader.sampler.weights = torch.softmax(torch.max(model.multipliers[0].weight, dim=-1).values, dim=0)
-
-    update_weights()
 
     step = 0
     optimizer = torch.optim.Adagrad(model.parameters(), lr=config.warmup_lr)
