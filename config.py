e467d7996cbf98c79af78a73d779232ce33f2014
manuel <manuel.delverme@gmail.com>
2021-04-19 21:59:11 +0200

    [CLUSTER] cifar10

--- config.py
+++ config.py
@@ -10,8 +10,10 @@ REMOTE = 1
 DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
 dataset_path = "../data" if DEBUG else "/network/datasets/{}.var/{}_torchvision"
 
-experiment = "robust_classification"  # "target_prop"
-dataset = "mnist"  # "cifar10"
+# experiment = "robust_classification"  # "target_prop"
+experiment = "target_prop"
+# dataset = "mnist"  # "cifar10"
+dataset = "cifar10"
 
 # Robust Classification experiments
 corruption_percentage = 0.00
