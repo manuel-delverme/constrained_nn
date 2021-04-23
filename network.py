6ed3590f118748a2388036a2057aff55c800b1fe
manuel <manuel.delverme@gmail.com>
2021-04-23 03:13:16 +0200

    [CLUSTER] mnist

--- network.py
+++ network.py
@@ -114,49 +114,11 @@ class CIAR10TargetProp(nn.Module):
         return x
 
 
-class MNSITNet(nn.Module):
-    def __init__(self, train_loader):
-        super().__init__()
-        self.block1 = nn.Sequential(
-            nn.Conv2d(1, 32, 3, 1),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, 3, 1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Flatten(1),
-            nn.Linear(9216, 128),
-            nn.ReLU(),
-            nn.Linear(128, 10),
-            nn.LogSoftmax(dim=1)
-        )
-        dataset_size = len(train_loader.dataset)
-        weight = torch.zeros(dataset_size, 1)
+class MNISTNetwork(TargetPropNetwork):
+    def forward(self, x0):
+        return self.full_rollout(x0)
 
-        with torch.no_grad():
-            for batch_idx, (data, target, indices) in tqdm.tqdm(enumerate(train_loader), total=len(train_loader)):
-                x_i = self.block1(data)
-                weight[indices] = x_i
 
-        self.x1 = nn.Sequential(
-            nn.Embedding(dataset_size, 128, _weight=weight, sparse=True),
-            nn.ReLU()
-        )
-        self.multipliers = nn.Sequential(
-            nn.Embedding(dataset_size, 128, _weight=torch.zeros(dataset_size, 128), sparse=True),
-        )
-
-    def forward(self, x0, indices):
-        x1_target = self.x1(indices)
-
-        x1_hat = self.block1(x0)
-        x_T = self.block3(x1_target)
-
-        h = x1_hat - x1_target
-
-        eps_h = F.softshrink(h, config.constr_margin)
-        return x_T, eps_h
-
-    def full_rollout(self, x):
-        x = self.block1(x)
-        x = self.block3(x)
-        return x
+class CIFAR10Network(CIAR10TargetProp):
+    def forward(self, x0):
+        return self.full_rollout(x0)
