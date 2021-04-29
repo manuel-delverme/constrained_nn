f653a7582d7b0d28eec29767cfba877341a7f2b8
manuel <manuel.delverme@gmail.com>
2021-04-30 00:06:15 +0200

    [CLUSTER] margin_sweep

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import torch
 
 import experiment_buddy
 
-RUN_SWEEP = 0
+RUN_SWEEP = 1
 REMOTE = 1
 
 DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
@@ -66,5 +66,5 @@ tb = experiment_buddy.deploy(
     extra_slurm_headers="""
     """,
     # SBATCH --mem=24GB
-    proc_num=10 if RUN_SWEEP else 1
+    proc_num=5 if RUN_SWEEP else 1
 )
