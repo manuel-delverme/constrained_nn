842fbb8b4a24459ef180ea470a10e9b73bcbdaaa
manuel <manuel.delverme@gmail.com>
2021-05-07 03:14:11 +0200

    target-prop_extra-gradient_mnist

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import torch
 
 import experiment_buddy
 
-RUN_SWEEP = 0
+RUN_SWEEP = 1
 REMOTE = 1
 
 DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
@@ -63,11 +63,22 @@ if distributional:
 # Derivative parameters
 ################################################################
 
-tb = experiment_buddy.deploy(
-    host="mila" if REMOTE else "",
-    sweep_yaml="sweep_hyper.yaml" if RUN_SWEEP else False,
-    extra_slurm_headers="""
-    """,
-    # SBATCH --mem=24GB
-    proc_num=10 if RUN_SWEEP else 1
-)
+try:
+    tb = experiment_buddy.deploy(
+        host="mila" if REMOTE else "",
+        sweep_yaml="sweep_hyper.yaml" if RUN_SWEEP else False,
+        extra_slurm_headers="""
+        """,
+        experiment_id="_".join((experiment, constraint_satisfaction, dataset)),
+        # SBATCH --mem=24GB
+        proc_num=1 if RUN_SWEEP else 1
+    )
+except Exception as e:
+    print(e, file=sys.stderr)
+    tb = experiment_buddy.deploy(
+        host="mila" if REMOTE else "",
+        sweep_yaml="sweep_hyper.yaml" if RUN_SWEEP else False,
+        extra_slurm_headers="""
+        """,
+        proc_num=1 if RUN_SWEEP else 1
+    )
