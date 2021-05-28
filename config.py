2645fa84563e2c1a54129654ce728b0c27db6ae7
manuel <manuel.delverme@gmail.com>
2021-05-28 06:20:14 +0200

    [CLUSTER] penalty_cifar

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ dataset_path = "../data" if DEBUG else "/network/datasets/{}.var/{}_torchvision"
 experiment = ["sgd", "target-prop"][1]
 constraint_satisfaction = ["penalty", "descent-ascent", "extra-gradient"][0]
 dataset = ["mnist", "cifar10"][1]
-distributional = True
+distributional = 0
 
 batch_size = 1024
 
