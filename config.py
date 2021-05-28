70dee3f7c20823af5df848580a90bbe8cd5aa121
manuel <manuel.delverme@gmail.com>
2021-05-28 06:44:58 +0200

    [CLUSTER] hail_mary

--- config.py
+++ config.py
@@ -101,5 +101,5 @@ tb = experiment_buddy.deploy(
     sweep_yaml="sweep_hyper.yaml" if RUN_SWEEP else False,
     extra_slurm_headers="""
     """,
-    proc_num=1 if RUN_SWEEP else 1
+    proc_num=20 if RUN_SWEEP else 1
 )
